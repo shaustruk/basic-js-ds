5c28f9244c68edaea21087d5b0d786b5508a9c79
KATSIARYNA <statsevich22@gmail.com>
2021-10-12 18:38:55 +0300

    init: restart project

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,57 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
+  node = null; //объявление корня, тк root занято
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+return this.node;  
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+}
+  add(data) {
+    const newNode = new Node(data); //создаем константу с новым деревом ,если при добавлении дерева еше нет
+
+    if(!this.node) {            //если корня еще нет, то корнем и будт новый добавленный элемнт data
+      this.node = newNode;      
+      return;
+    }
+     //ищеи место, куда надо вставить листочек, больше-меньше
+     let currentNode = this.node;  //текущий элемент - позиция, который будет ходить// начальный элемент корень - root (у нас node)
+     while (currentNode) {  //пока в дереве есть элементы идем=>
+      if (newNode.data < currentNode.data) {   //если добавляемое < текущего( сначала это корень)
+        if(!currentNode.left) { //если слева от тек ничего нет,  то добавляем туда
+          currentNode.letf = newNode;
+            return; 
+          }
+          currentNode = currentNode.left;
+        }
+        else {                  //если добавляемое больше, то=>
+          if(!currentNode.right) {  //если справа от корня ничего нет - добавляем туда
+            currentNode.right = newNode;
+            return;
+          }
+          currentNode = currentNode.right;
+        }   
+      }
+   
   }
 
   has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   
   }
 
   find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
   }
 
   remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   
   }
 
 }
