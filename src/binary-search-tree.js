e13cf9f6cb191ff2e72880d47a13f3f3691542e8
KATSIARYNA <statsevich22@gmail.com>
2021-10-13 11:24:52 +0300

    feat: add code to tack BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,18 +1,18 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const { Node } = require('../extensions/list-tree.js');
+/*const { Node } = require('../extensions/list-tree.js');*/
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-/*class Node {
+class Node {
 	constructor(data = null) {
 		this.data = data;
 		this.right = null;
 		this.left = null;
 	}
-}*/
+}
 module.exports = class BinarySearchTree {
 
   node = null; //объявление корня, тк root занято
@@ -65,21 +65,99 @@ return this.node;
 
   }
   }
-
+/*------------------------*/
   find(data) {
-  
-  }
+		return findWithIn(this.node, data); // возвращаем результат вызова ф-ции, которой говорим, где мы ищем - начинаем с корня дерева и что мы ищем - data
+		function findWithIn (current, data) { //текущий и проверяемый
+	   
+		if (!current) { //если нет узлов
+		  return null;
+		}
+	
+		if (current.data === data) { //значения совпали => true
+		  return current;
+		}
+		return data < current.data ?
+		findWithIn(current.left, data) :
+		findWithIn(current.right, data);
+	
+	  }
+}
+/*---------------------*/
 
   remove(data) {
-   
+    this.node = removeNode(this.node, data); // положить в корень результат выполнения ф-ции => где удалить и с каким значением   ***
+		function removeNode(current, data) {
+		if (!current) {  //если там нал - нет узла, то этот нал и оставляем
+				return null; //точка остановки рекурсии
+			}
+			//определяем вкаку сторону пойти
+		if(data < current.data) {//искомое меньше знач в тек узле => идем к леврму узлу
+			current.left = removeNode(current.left, data ); // удаляем левый узел и то, что получится положить без удал эл-та
+			return current; // возвращаем текущий узул, чтобы мы положиди его в наш корень node (наверх)***
+		}
+
+		if (data > current.data) {//искомое > знач в тек узле => идем к right узлу
+			current.right = removeNode(current.right, data ); // удаляем right узел и то, что получится положить без удал эл-та    
+			return current;    /* ***  и верни обновленное поддерево в корень*/
+			}
+
+			//поверелили, что узел сущ, не >, не <       ===> равен искомому
+		else {
+			if(!current.left && !current.right) {       //проверим явл ли он листом и его удалим
+				return null;
+
+			}  
+			if (!current.left) {       //если нет лев потомка 
+				current = current.right;  //вместо нас помещаем правого потомка
+				return current;        //возвращаем обновленное поддерево в корень
+			}              
+			if (!current.right) {       //если нет right потомка 
+				current = current.left;  //вместо нас помещаем left потомка
+				return current;        //возвращаем обновленное поддерево в корень
+			}              
+			// у искомого (найденного) есть оба поддерева
+			let minFromRight = current.right; //ищеи мин среди правого
+			while(minFromRight.left) { //пока элемент слева есть мы перемещаемся к нему
+				minFromRight = minFromRight.left;
+			
+			}
+			current.data= minFromRight.data; // как только нашли мин эл в правом поддереве помещаем это знач в знач удаляемго узла
+			current.right = removeNode(current.right, minFromRight.data);
+			return current;
+
+
+		}
+	}
+}
+
+min() {
+  if (!this.node){
+    return;
   }
 
-  min() {
-    
-  }
+  let current = this.node; //вводим переменную для поиска мин
+  while(current.left) { //пока слева кто-то есть
+    current = current.left; // перемещаемся влево
 
-  max() {
-   
   }
+  return current.data;  //как только ксе закончились, возвращаем посл эл
+
+   
+}
+
+max() {
+  if (!this.node){
+    return;
+  }
+  let current = this.node; //вводим переменную для поиска мин
+  while(current.right) { //пока справа кто-то есть
+    current = current.right;
+
+  }
+  return current.data;
+
+ 
+}
 
 }
